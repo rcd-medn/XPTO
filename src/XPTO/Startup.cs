0f4363aad1a0ff8df132a66698e10451ba600b78
Ricardo <para.sites.estudos@gmail.com>
2021-03-03 18:03:22 -0300

    Criada a string de conexão como banco de dados e adicionada ao arquivo appsettings.json também foi resgitrado o serviço na classe Startup.

--- src/XPTO/Startup.cs
+++ src/XPTO/Startup.cs
@@ -1,3 +1,7 @@
+
+
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,6 +9,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using XPTO.Data;
@@ -13,6 +19,13 @@ namespace XPTO
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,6 +39,14 @@ namespace XPTO
             // Registra o serviço (classe) necessário para a aplicação.
             // ============================================================================================================================
             services.AddScoped<IClienteAPIRepository, MockClienteAPIRepository>();
+
+            // ============================================================================================================================
+            // Registra o serviço (classe) DBContext para acessar o banco de dados.
+            // ============================================================================================================================
+            services.AddDbContext<XPTOContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("XPTO"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
